cf21b4487b9f0f1c26ec75ee2c2c7e22d1fa271f
kitsuiwebster <raphmartin1999@gmail.com>
2026-02-23 13:45:16 +0000

    fix: tg chat id import

--- app/config.py
+++ app/config.py
@@ -1,6 +1,9 @@
 # Global configuration values.
 import os
 
+from dotenv import load_dotenv
+
 
 # Whitelist of authorized chat IDs
+load_dotenv()
 AUTHORIZED_CHAT_IDS = [os.getenv("TELEGRAM_CHAT_ID")]
